c6e4810f799e947551bf5a08c3b31379672e8bd0
neebneeba <neba0317@gmail.com>
2024-07-09 20:12:03 +0900

    footer finished

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -1,7 +1,12 @@
 import { FC } from "react";
 
 const Footer: FC = () => {
-  return <div>this is Footer</div>;
+  return (
+    <div className="mt-10 flex justify-center bg-black bg-opacity-60 py-5 text-white">
+      Copyright &copy; {new Date().getFullYear()}; Designed by&nbsp;
+      <strong>neba0317</strong>
+    </div>
+  );
 };
 
 export default Footer;
