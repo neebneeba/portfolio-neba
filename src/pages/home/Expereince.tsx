295912dbb0cbd0c4b2698d64dd8e16911e150ff7
neebneeba <neba0317@gmail.com>
2024-07-10 22:52:18 +0900

    soft master translation added to an array

--- src/pages/home/Expereince.tsx
+++ src/pages/home/Expereince.tsx
@@ -4,37 +4,36 @@ import { useTranslation } from "react-i18next";
 // Components
 import ExperienceItem from "../../components/ExperienceItem";
 
-const experiences: Array<{
-  date: string;
-  title: string;
-  subtitle: string;
-  paragraph: string;
-}> = [
-  {
-    date: "2019 / 12",
-    title: "FullStack Engineer",
-    subtitle: "SoftMaster LLC",
-    paragraph:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae amet saepe laboriosam nostrum quaerat ducimus necessitatibus rerum? Ullam fuga reiciendis repellat vitae architecto sequi excepturi quidem illo sunt? Quis animi vel quae labore doloremque iure nam fuga possimus nobis inventore vero exercitationem atque accusantium, est quos laudantium laborum tempore! Eius voluptate quos maxime sit ea molestias consequatur enim fuga, corporis, aperiam provident voluptatem consequuntur. Voluptatem, autem atque. Officia rerum adipisci, autem vero, eveniet velit necessitatibus itaque unde accusamus cupiditate consequatur labore error corrupti dignissimos repudiandae nihil minus alias magnam tempore eius. Totam quos vitae veniam doloremque aliquam alias debitis ullam?",
-  },
-  {
-    date: "2021 / 08",
-    title: "Frontend Engineer",
-    subtitle: "ICT Group",
-    paragraph:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae amet saepe laboriosam nostrum quaerat ducimus necessitatibus rerum? Ullam fuga reiciendis repellat vitae architecto sequi excepturi quidem illo sunt? Quis animi vel quae labore doloremque iure nam fuga possimus nobis inventore vero exercitationem atque accusantium, est quos laudantium laborum tempore! Eius voluptate quos maxime sit ea molestias consequatur enim fuga, corporis, aperiam provident voluptatem consequuntur. Voluptatem, autem atque. Officia rerum adipisci, autem vero, eveniet velit necessitatibus itaque unde accusamus cupiditate consequatur labore error corrupti dignissimos repudiandae nihil minus alias magnam tempore eius. Totam quos vitae veniam doloremque aliquam alias debitis ullam?",
-  },
-  {
-    date: "2022 / 04",
-    title: "FullStack Engineer",
-    subtitle: "Unitel Group",
-    paragraph:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae amet saepe laboriosam nostrum quaerat ducimus necessitatibus rerum? Ullam fuga reiciendis repellat vitae architecto sequi excepturi quidem illo sunt? Quis animi vel quae labore doloremque iure nam fuga possimus nobis inventore vero exercitationem atque accusantium, est quos laudantium laborum tempore! Eius voluptate quos maxime sit ea molestias consequatur enim fuga, corporis, aperiam provident voluptatem consequuntur. Voluptatem, autem atque. Officia rerum adipisci, autem vero, eveniet velit necessitatibus itaque unde accusamus cupiditate consequatur labore error corrupti dignissimos repudiandae nihil minus alias magnam tempore eius. Totam quos vitae veniam doloremque aliquam alias debitis ullam?",
-  },
-];
-
 const Experience: FC = () => {
   const { t } = useTranslation();
+
+  let experiences: Array<{
+    date: string;
+    title: string;
+    companyName: string;
+    paragraph: string;
+  }> = [
+    {
+      date: "2019 / 12",
+      title: t("experience.softMaster.title"),
+      companyName: t("experience.softMaster.companyName"),
+      paragraph: t("experience.softMaster.paragraph"),
+    },
+    {
+      date: "2021 / 08",
+      title: "Frontend Engineer",
+      companyName: "ICT Group",
+      paragraph:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae amet saepe laboriosam nostrum quaerat ducimus necessitatibus rerum? Ullam fuga reiciendis repellat vitae architecto sequi excepturi quidem illo sunt? Quis animi vel quae labore doloremque iure nam fuga possimus nobis inventore vero exercitationem atque accusantium, est quos laudantium laborum tempore! Eius voluptate quos maxime sit ea molestias consequatur enim fuga, corporis, aperiam provident voluptatem consequuntur. Voluptatem, autem atque. Officia rerum adipisci, autem vero, eveniet velit necessitatibus itaque unde accusamus cupiditate consequatur labore error corrupti dignissimos repudiandae nihil minus alias magnam tempore eius. Totam quos vitae veniam doloremque aliquam alias debitis ullam?",
+    },
+    {
+      date: "2022 / 04",
+      title: "FullStack Engineer",
+      companyName: "Unitel Group",
+      paragraph:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae amet saepe laboriosam nostrum quaerat ducimus necessitatibus rerum? Ullam fuga reiciendis repellat vitae architecto sequi excepturi quidem illo sunt? Quis animi vel quae labore doloremque iure nam fuga possimus nobis inventore vero exercitationem atque accusantium, est quos laudantium laborum tempore! Eius voluptate quos maxime sit ea molestias consequatur enim fuga, corporis, aperiam provident voluptatem consequuntur. Voluptatem, autem atque. Officia rerum adipisci, autem vero, eveniet velit necessitatibus itaque unde accusamus cupiditate consequatur labore error corrupti dignissimos repudiandae nihil minus alias magnam tempore eius. Totam quos vitae veniam doloremque aliquam alias debitis ullam?",
+    },
+  ];
 
   return (
     <div className="bg-stone-700">
@@ -51,7 +50,7 @@ const Experience: FC = () => {
               key={useId()}
               date={experience.date}
               title={experience.title}
-              subtitle={experience.subtitle}
+              companyName={experience.companyName}
               paragraph={experience.paragraph}
             />
           ))}
