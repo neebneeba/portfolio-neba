5dc641dabb2395fb09e9c609def3381533b11f1a
neebneeba <neba0317@gmail.com>
2024-07-15 15:15:17 +0900

    Skill components responsive

--- src/components/Skill.tsx
+++ src/components/Skill.tsx
@@ -2,13 +2,19 @@ import { FC, useId } from "react";
 
 const Skill: FC<{ icon: Array<string>; title: string }> = (props) => {
   return (
-    <div className="flex flex-col space-y-2 rounded-lg bg-gradient-to-tr from-stone-700 to-slate-700 py-32 transition-all duration-200 hover:scale-105">
-      <div className="m-auto flex space-x-2">
+    <div className="flex flex-col space-y-2 rounded-lg bg-gradient-to-tr from-stone-700 to-slate-700 py-8 transition-all duration-200 hover:scale-105 sm:py-24 xl:py-32">
+      <div className="mx-auto flex space-x-2">
         {props.icon.map((item) => (
-          <img src={item} key={useId()} />
+          <img
+            src={item}
+            key={useId()}
+            className="h-5 w-5 md:h-10 md:w-10 lg:h-20 lg:w-20"
+          />
         ))}
       </div>
-      <h5 className="mx-auto font-oswald font-bold">{props.title}</h5>
+      <h5 className="mx-auto font-oswald text-xs font-semibold md:text-base md:font-bold">
+        {props.title}
+      </h5>
     </div>
   );
 };
