3bff4d3bd6adcf8eae75f5ebebbfdd91e64a75c2
neebneeba <neba0317@gmail.com>
2024-07-15 15:15:34 +0900

    Header components responsive

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -8,18 +8,27 @@ const Header: FC = () => {
   const { t } = useTranslation();
 
   return (
-    <div className="container mb-20 flex justify-between py-5">
-      <div className="my-auto flex flex-row space-x-20">
-        <a href="" className="font-lgxw text-2xl font-semibold">
+    <div className="container mb-20 hidden justify-between py-5 md:flex md:px-5">
+      <div className="my-auto flex flex-row space-x-20 md:space-x-5">
+        <a href="/" className="font-lgxw text-xl font-semibold lg:text-2xl">
           {t("sections.home")}
         </a>
-        <a href="#skills" className="font-lgxw text-2xl font-semibold">
+        <a
+          href="#skills"
+          className="font-lgxw text-xl font-semibold lg:text-2xl"
+        >
           {t("sections.skills")}
         </a>
-        <a href="#experience" className="font-lgxw text-2xl font-semibold">
+        <a
+          href="#experience"
+          className="font-lgxw text-xl font-semibold lg:text-2xl"
+        >
           {t("sections.experience")}
         </a>
-        <a href="#projects" className="font-lgxw text-2xl font-semibold">
+        <a
+          href="#projects"
+          className="font-lgxw text-xl font-semibold lg:text-2xl"
+        >
           {t("sections.projects")}
         </a>
       </div>
